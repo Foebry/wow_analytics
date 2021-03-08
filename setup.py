5021eea0592ccb9860305434d03f79873f206430
Foebry <rain_fabry@hotmail.com>
2021-03-08 20:01:51 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup 
+setup( 
+    name = 'Wow_Analytics', 
+    version = 1.0.0, 
+    packages = [], 
+    url = 'https://github.com/Foebry/Wow_Analytics', 
+    author = 'Foebry', 
+    author_email = 'rain_fabry@hotmail.com', 
+    description = '' 
+) 
