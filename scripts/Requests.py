7f1b4743cac0b3aa5a01460f0ffbfed0510a6cb6
Foebry <rain_fabry@hotmail.com>
2021-03-10 21:39:05 +0100

    implemented requests functionality with tests

--- scripts/Requests.py
+++ scripts/Requests.py
@@ -1,1 +1,82 @@
-ECHO is on.
+"""Requests functionalities"""
+import requests
+
+
+class Request():
+    """ """
+    def __init__(self, data):
+        """Request constructor"""
+        self.client_id = data['id']
+        self.client_secret = data['secret']
+        self.access_token = self.getAccesToken()
+
+    def getAccesToken(self):
+        """getting access_token"""
+        # setting required data
+        address = "https://us.battle.net/oauth/token"
+        data = {
+            'grant_type':'client_credentials',
+            'client_id': self.client_id ,
+            'client_secret': self.client_secret
+        }
+        # making request
+        response = requests.post(address, data)
+
+        # checking response
+        if response.status_code == 200: return response.json()['access_token']
+        else: logger.log('error', f'{response.status_code} - response')
+
+    def getAuctionData(self, realm_id):
+        """getting auction data"""
+        address = f"https://eu.api.blizzard.com/data/wow/connected-realm/{realm_id}/auctions?namespace=dynamic-eu&locale=en_GB&access_token={self.access_token}"
+        response = requests.get(address)
+        if response.status_code == 200: return response.json()['auctions']
+        elif response.status_code == 401:
+            self.access_token = self.getAccesToken()
+            self.getAuctionData(realm_id)
+
+
+    def getItemData(self, item):
+        """getting item data"""
+        address = f"https://eu.api.blizzard.com/data/wow/item/{item}?namespace=static-eu&locale=en_GB&access_token={self.access_token}"
+        response = requests.get(address)
+        if response.status_code == 200: return response.json()
+        elif response.status_code == 401:
+            self.access_token = self.getAccesToken()
+            self.getItemData(item)
+
+    def getClassData(self, class_id):
+        """getting class data"""
+        address = f"https://eu.api.blizzard.com/data/wow/item-class/{class_id}?namespace=static-eu&locale=en_GB&access_token={self.access_token}"
+        response = requests.get(address)
+        if response.status_code == 200: return response.json()
+        elif response.status_code == 401:
+            self.access_token = self.getAccesToken()
+            self.getClassData(class_id)
+
+    def getSubclassData(self, class_id, subclass):
+        """getting subclass data"""
+        address = f"https://eu.api.blizzard.com/data/wow/item-class/{class_id}/item-subclass/{subclass}?namespace=static-eu&locale=en_GB&access_token={self.access_token}"
+        response = requests.get(address)
+        if response.status_code == 200: return response.json()
+        elif response.status_code == 401:
+            self.access_token = self.getAccesToken()
+            self.getSubclassData(class_id, subclass)
+
+    def getPetData(self, pet):
+        """getting pet data"""
+        address = f"https://eu.api.blizzard.com/data/wow/pet/{pet}?namespace=static-eu&locale=en_GB&access_token={self.access_token}"
+        response = requests.get(address)
+        if response.status_code == 200: return response.json()
+        elif response.status_code == 401:
+            self.access_token = self.getAccesToken()
+            self.getPetData(pet)
+
+    def getMountData(self, mount):
+        """getting mount data"""
+        address = f"https://eu.api.blizzard.com/data/wow/mount/{mount}?namespace=static-eu&local=en_GB&access_token={self.access_token}"
+        response = requests.get(address)
+        if response.status_code == 200: return response.json()
+        elif response.status_code == 401:
+            self.status_code = self.getAccesToken()
+            self.getMountData(mount)
--- tests/requests_test.py
+++ tests/requests_test.py
@@ -1,1 +1,89 @@
-ECHO is on.
+"""Requests testing functionalities"""
+import unittest
+from Wow_Analytics.scripts.Requests import Request
+
+
+
+class RequestTest(unittest.TestCase):
+
+    def test_init(self):
+        """testing init method for Request"""
+        self.assertEqual(type(str()),type(request.access_token))
+        self.assertEqual('2d145be238fc4068a18cd9a2cb7473eb', request.client_id)
+        self.assertEqual('m6eqhka7BWQxVJc9dYn2cO70zLYHE2uo', request.client_secret)
+
+    def test_getAuctionData(self):
+        """testing getAuctionData method for Request"""
+        self.assertEqual(type(list()), type(request.getAuctionData(1096)))
+
+    def test_getItemData(self):
+        """testing getItemData method for Request object"""
+        # making requests
+        response = request.getItemData(35)
+
+        # checking response
+        self.assertEqual(type(dict()), type(response))
+        self.assertEqual(35, response['id'])
+        self.assertEqual("Bent Staff", response["name"])
+        self.assertEqual("COMMON",response["quality"]["type"])
+        self.assertEqual("Common", response["quality"]["name"])
+        self.assertEqual("Weapon", response["item_class"]["name"])
+        self.assertEqual("Staff", response["item_subclass"]["name"])
+
+    def test_getClassData(self):
+        """testing getClassData for Request object"""
+        # making request
+        response = request.getClassData(0)
+
+        # checking response
+        self.assertEqual(type(dict()), type(response))
+        self.assertEqual(0, response["class_id"])
+        self.assertEqual("Consumable", response["name"])
+
+    def test_getSubclassData(self):
+        """testing getSubclassData for Request object"""
+        # making request
+        response = request.getSubclassData(0, 0)
+
+        # checking response
+        self.assertEqual(type(dict()), type(response))
+        self.assertEqual(0, response["class_id"])
+        self.assertEqual(0, response["subclass_id"])
+        self.assertEqual("Explosives and Devices", response["display_name"])
+
+    def test_getPetData(self):
+        """testing getPetData for Request object"""
+        # making request
+        response = request.getPetData(39)
+
+        # checking response
+        self.assertEqual(type(dict()), type(response))
+        self.assertEqual(39, response["id"])
+        self.assertEqual("Mechanical Squirrel", response["name"])
+        self.assertEqual(9, response["battle_pet_type"]["id"])
+        self.assertEqual("MECHANICAL", response["battle_pet_type"]["type"])
+        self.assertEqual("Mechanical", response["battle_pet_type"]["name"])
+
+
+    def test_getMountData(self):
+        """testing getMountData for Request object"""
+        # making request
+        response = request.getMountData(6)
+
+        # checking response
+        self.assertEqual(type(dict()), type(response))
+        self.assertEqual(6, response["id"])
+        self.assertEqual("Brown Horse", response["name"]["en_US"])
+        self.assertEqual("VENDOR", response["source"]["type"])
+        self.assertEqual("Vendor", response["source"]["name"]["en_US"])
+
+
+
+if __name__ == '__main__':
+    # creating Request object
+    data = {
+        'id': '2d145be238fc4068a18cd9a2cb7473eb',
+        'secret': 'm6eqhka7BWQxVJc9dYn2cO70zLYHE2uo'
+    }
+    request = Request(data)
+    unittest.main()
