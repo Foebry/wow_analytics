43c07b305e32e10361612cf60608ac59c921dd5b
Foebry <rain_fabry@hotmail.com>
2021-04-02 18:00:12 +0200

    rewind

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name = 'Wow_Analytics',
-    version = 1.1, 
+    version = 1.0, 
     packages = [],
     url = 'https://github.com/Foebry/Wow_Analytics',
     author = 'Foebry',
