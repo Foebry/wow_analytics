56618b28b9a52ab2e95f20a915e8efb928d5fa1b
Foebry <rain_fabry@hotmail.com>
2021-04-02 17:54:33 +0200

    Verion 1.1

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup 
-setup( 
-    name = 'Wow_Analytics', 
-    version = 1.0.0, 
-    packages = [], 
-    url = 'https://github.com/Foebry/Wow_Analytics', 
-    author = 'Foebry', 
-    author_email = 'rain_fabry@hotmail.com', 
-    description = '' 
-) 
+from setuptools import setup
+setup(
+    name = 'Wow_Analytics',
+    version = 1.1, 
+    packages = [],
+    url = 'https://github.com/Foebry/Wow_Analytics',
+    author = 'Foebry',
+    author_email = 'rain_fabry@hotmail.com',
+    description = ''
+)
