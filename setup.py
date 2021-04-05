1b8ffcd262ba243e3c81cb14e784b7ebc2914814
Foebry <rain_fabry@hotmail.com>
2021-04-05 20:47:23 +0200

    ignoring config.py

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
 setup(
     name = 'Wow_Analytics',
-    version = 1.1, 
-    packages = [],
+    version = 1.1,
+    packages = ['Wow_Analytics'],
+    install_requires = [
+        'Databases @ git+https://github.com/Foebry/Databases.git#egg=Databases',
+        'Logger @ git+https://github.com/Foebry/Databases.git#egg=Logger',
+        'requires',
+        'mysql-connector'
+    ],
     url = 'https://github.com/Foebry/Wow_Analytics',
     author = 'Foebry',
     author_email = 'rain_fabry@hotmail.com',
