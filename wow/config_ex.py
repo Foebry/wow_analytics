e680429a98997486ac2d1a6e6a4e188e2e1cd9dc
Foebry <rain_fabry@hotmail.com>
2021-04-15 15:03:18 +0200

    added config_ex

---
+++ wow/config_ex.py
@@ -0,0 +1,14 @@
+DATABASE = {
+    'user': 'MyConnectionName',
+    'password': 'MyPassword',
+    'host': 'MyDB_IP_Address',
+    'schema': 'MySchemaName'
+}
+
+REALMS = {
+    'MyRealm': {
+        'id': 'MyRealmID' # int,
+        'client_id': 'MyClientID',
+        'client_secret': 'MyClientSecret'
+    }
+}
